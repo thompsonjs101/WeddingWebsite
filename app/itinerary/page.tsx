44e636d4782ceb4ed2d18f991ff649911c2c6100
Joel Thompson <thompsonjs101@gmail.com>
2025-11-12 19:37:19 +0100

    Refactor hotel links into list format
    Updated hotel options in itinerary with list format.

--- app/itinerary/page.tsx
+++ app/itinerary/page.tsx
@@ -66,10 +66,25 @@ export default function ItineraryPage() {
               <div className="px-3 md:px-6">
                 <p className="text-muted-foreground mb-4 text-sm md:text-base">
                   We'll be arriving in Madrid at 7pm and checking into our hotel. We will be staying at one of the following hotels:
-                  <a href= "https://www.booking.com/hotel/es/crosstown-by-pillow.no.html?aid=898224&app_hotel_id=9662096&checkin=2026-06-17&checkout=2026-06-20&from_sn=ios&group_adults=2&group_children=0&label=hotel_details-5RQ1kwZ%401762875376&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA%2C&chal_t=1762971128862&force_referer="> BYPILLOW Crosstown </a>
-                  <a href= "https://www.booking.com/hotel/es/b-amp-b-puerta-del-sol.no.html?aid=898224&label=hotel_details-G1uxJ9K%401762886716&sid=c23675ec250cebafd590b8c7f6fcf246&checkin=2026-06-17&checkout=2026-06-20&dist=0&from_sn=ios&group_adults=2&group_children=0&keep_landing=1&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA%2C&sb_price_type=total&type=total&"> B&B HOTEL Madrid Centro Puerta del Sol </a>
-                  <a href= "https://www.booking.com/hotel/es/villadelareina.no.html?fbclid=IwY2xjawOAcXhleHRuA2FlbQIxMABicmlkETFsSkt4VVFZV3pYb2tSVkVvc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHn-5Liz50ONzUlHj04fafB8EjS1hG26x_x5_TySgWI_wx8CSupc_kNPGlUKl_aem_ybs3Tdo88BlGLnykdjDMhw&chal_t=1762890281841&force_referer=https%3A%2F%2Fl.facebook.com%2F"> Boutique Hotel H10 Villa de la Reina </a>
-                  We will be checking out the first two hotels in March but all are in the Sivilla area.
+                  <li className="flex items-start gap-2">
+                      <span className="text-rose-400">•</span>
+                      <span>
+                       <a href= "https://www.booking.com/hotel/es/crosstown-by-pillow.no.html?aid=898224&app_hotel_id=9662096&checkin=2026-06-17&checkout=2026-06-20&from_sn=ios&group_adults=2&group_children=0&label=hotel_details-5RQ1kwZ%401762875376&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA%2C&chal_t=1762971128862&force_referer="> BYPILLOW Crosstown </a> 
+                      </span>
+                  </li>
+                  <li className="flex items-start gap-2">
+                      <span className="text-rose-400">•</span>
+                      <span>
+                       <a href= "https://www.booking.com/hotel/es/b-amp-b-puerta-del-sol.no.html?aid=898224&label=hotel_details-G1uxJ9K%401762886716&sid=c23675ec250cebafd590b8c7f6fcf246&checkin=2026-06-17&checkout=2026-06-20&dist=0&from_sn=ios&group_adults=2&group_children=0&keep_landing=1&no_rooms=1&req_adults=2&req_children=0&room1=A%2CA%2C&sb_price_type=total&type=total&"> B&B HOTEL Madrid Centro Puerta del Sol </a>
+                      </span>
+                  </li>
+                  <li className="flex items-start gap-2">
+                      <span className="text-rose-400">•</span>
+                      <span>
+                      <a href= "https://www.booking.com/hotel/es/villadelareina.no.html?fbclid=IwY2xjawOAcXhleHRuA2FlbQIxMABicmlkETFsSkt4VVFZV3pYb2tSVkVvc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHn-5Liz50ONzUlHj04fafB8EjS1hG26x_x5_TySgWI_wx8CSupc_kNPGlUKl_aem_ybs3Tdo88BlGLnykdjDMhw&chal_t=1762890281841&force_referer=https%3A%2F%2Fl.facebook.com%2F"> Boutique Hotel H10 Villa de la Reina </a>
+                      </span>
+                  </li>
+                  We will be checking out the first two hotels in March but all are in the Sevilla area.
                   After we have checked in we will be going out for a quick bite and some drinks.
                 </p>
               </div>
